62d1ae3f842300354efe0c6d5153ceb2be826780
Josh Lee <josh.lee2415@gmail.com>
2021-02-04 14:07:51 +1100

    Attempting diversity indices

--- R/birds/Diversity.R
+++ R/birds/Diversity.R
@@ -1,42 +1,43 @@
-library(ggplot2)
 library(tidyverse)
+library(vegan)
+library(labdsv)
+
 
 #Species Richness
 
 birds <- read.csv("Data/Raw/Birds.csv")
 birds$Treatment<- with(birds, paste0(Formation, Fire))
 birds <- birds %>%
-  group_by(Site) %>%
-  mutate(species_richness = n_distinct(Species))
+  filter(Notes != "Out of Survey") %>% 
+  dplyr::group_by(Site) %>%
+  dplyr::mutate(species_richness = n_distinct(Species))
 
-sr <- ggplot(birds, aes(x = Treatment, y = species_richness)) + geom_boxplot() +ylab("# species")+ ggtitle("All")
+
+#plotting sr
+ggplot(birds, aes(x = Treatment, y = species_richness)) + geom_boxplot() +ylab("# species")+ ggtitle("All")
 ggplot(birds, aes(x = Location, y = species_richness, fill = Treatment)) + geom_boxplot() +ylab("# species")
 
-birds100 <- filter(birds, Distance<=100)
-birds100 <- birds100 %>%
-  group_by(Site) %>%
-  mutate(species_richness = n_distinct(Species))
-sr100 <- ggplot(birds100, aes(x = Treatment, y = species_richness)) + geom_boxplot() +ylab("# species") + ggtitle("<=100")
-write.csv(birds100, file = "Data/Processed/birds100.csv")
-
-birds80 <- filter(birds, Distance<=80)
-birds80 <- birds80 %>%
-  group_by(Site) %>%
-  mutate(species_richness = n_distinct(Species))
-sr80 <- ggplot(birds80, aes(x = Treatment, y = species_richness)) + geom_boxplot() +ylab("# species")+ ggtitle("<=80")
-
-
-birds50 <- filter(birds, Distance<=50)
-birds50 <- birds50 %>%
-  group_by(Site) %>%
-  mutate(species_richness = n_distinct(Species))
-sr50<- ggplot(birds50, aes(x = Treatment, y = species_richness)) + geom_boxplot() +ylab("# species")+ ggtitle("<=50")
-
-print(sr)
-print(sr100)
-print(sr80)
-print(sr50)
-
+#investigating cutoff
 meandistances<- aggregate(birds[12], list(birds$Site, birds$Treatment), mean, na.rm = TRUE)
 distplot<- ggplot(birds, aes(x = Treatment, y = Distance)) + geom_boxplot() +ylab("Distance")+ ggtitle("Bird Distances")
 print(distplot)
+##### 100m looks like it gets rid of the outliers
+
+#crop to 100m
+birds100 <- filter(birds, Distance<=100)
+birds100 <- birds100 %>%
+  group_by(Site) %>%
+  dplyr::mutate(species_richness = n_distinct(Species))
+sr100 <- ggplot(birds100, aes(x = Treatment, y = species_richness)) + geom_boxplot() +ylab("# species") + ggtitle("<=100")
+write.csv(birds100, file = "Data/Processed/birds100.csv")
+
+#reorganising df
+birdspread<- birds100 %>% 
+  select(Site, Species, Count) %>% 
+  group_by(Site, Species) %>% 
+  summarise_all(funs(sum)) %>% 
+  spread(Species, Count, fill = 0)
+
+#calculating diversity index
+vegan::diversity(birdspread, index = "shannon")
+
