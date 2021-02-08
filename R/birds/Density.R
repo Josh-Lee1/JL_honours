907ba83876417c7fda13a4e74fcfa2f6e2d13e7c
Josh Lee <josh.lee2415@gmail.com>
2021-02-08 18:27:16 +1100

    Getting density to run

--- R/birds/Density.R
+++ R/birds/Density.R
@@ -12,6 +12,14 @@ birds <- birds %>%
   as.data.frame()
 birds$Area <- '628'
 birds$Effort <- '1'
+birds$Sample.Label <- '1'
+birds$Area <- as.numeric(birds$Area)
+birds$Effort <- as.numeric(birds$Effort)
+
+siteinfo <- birds %>% 
+  select(Region.Label, Fire, Formation, Treatment) %>% 
+  distinct() %>% 
+  rename(Site = Region.Label)
 
 #simple Abundance plot
 birds100 <- birds100 %>%
@@ -28,16 +36,34 @@ print(totbirds100)
 #birds_det <- ds(birds, truncation = 400,  formula = ~ Treatment)
 #birds_hr <- ds(birds, truncation = 400, key = "hr")
 #birds_hr_tre <- ds(birds, truncation = 400, key = "hr",  formula = ~ Treatment)
-#birds_hr_loc <- ds(birds, truncation = 400, key = "hr",  formula = ~ Location)
-birds_hr_sit <- ds(birds, truncation = 400, key = "hr",  formula = ~ Region.Label)
+birds_hr_loc <- ds(birds, truncation = 400, key = "hr",  formula = ~ Location)
+#summarize_ds_models(birds_hr_loc, birds_det, birds_hr, birds_hr_tre)
 
 plot(birds_hr_loc)
 summary(birds_hr_loc)
 gof_ds(birds_hr_loc)
-summarize_ds_models(birds_hr_loc, birds_det, birds_hr, birds_hr_tre)
+
 
 #summary to get abundance and density
 summary(birds_hr_loc)
-######### abundance and density not in here??
 
-birds_hr_loc$ddf
+abund_table <- summary(birds_hr_loc)$dht$individuals$N
+abund_table$lcl <- abund_table$ucl <- abund_table$df <- NULL
+colnames(abund_table) <- c("Site", "Estimate_abundance", "se_abundance",
+                           "CV_abundance")
+
+dense_table <- summary(birds_hr_loc)$dht$individuals$D
+dense_table$lcl <- dense_table$ucl <- dense_table$df <- NULL
+colnames(dense_table) <- c("Site", "Estimate_density", "se_density",
+                           "CV_density")
+density <- abund_table %>%
+  left_join(dense_table, by = "Site") %>% 
+  left_join(siteinfo, by = "Site") %>% 
+  filter(Site != "Total")
+
+#Plot and ANOVA
+abundplot<- ggplot(density, aes(x = Treatment, y = Estimate_abundance)) + geom_boxplot() +ylab("Abundance")
+print(abundplot)
+
+abund.aov<- aov(Estimate_abundance ~ Fire * Formation, data = density)
+summary(abund.aov)
